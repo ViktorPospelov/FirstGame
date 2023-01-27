f84d91090300e44204875baa0ffc03db811c57f4
Viktor <Vik.posp@gmail.com>
2023-01-27 16:36:41 +0500

    12

--- MyYaGame/Klondike/Assets/Script/Game/Bed/CardBed.cs
+++ MyYaGame/Klondike/Assets/Script/Game/Bed/CardBed.cs
@@ -1,29 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardBed : MonoBehaviour,IDropHandler
+public class CardBed : MonoBehaviour, IDropHandler
 {
     public int startingNumberOfCards;
     public float CardIndentation = 0f;
-    private float _currentIndent = 0f;
+    private List<Card> _cards = new List<Card>();
 
-    public void InsertStartCard(Card insertCard)
+    public void InsertStartCard(Card insertCard)//устоновить карту 
     {
-        var card = Instantiate(insertCard.gameObject).GetComponent<Card>();
-        card.gameObject.transform.SetParent(transform, false);
-        card.gameObject.transform.Translate(new Vector3(0, 
-            card.gameObject.transform.position.y+_currentIndent));
-        _currentIndent -= CardIndentation;
-    }
-    public void OnDrop(PointerEventData eventData)
-    {
-        var item = Card.dragItem;
-        if (item!=null)
+        var card = Instantiate(insertCard.gameObject).GetComponent<Card>(); //встваить карту
+
+        if (_cards.Count>0)//если карты в колоде есть
         {
-            item.transform.SetParent(transform);
+            card.gameObject.transform.SetParent(_cards[_cards.Count-1].gameObject.transform,false);//встваить в другую карту
+        }
+        else//если нету
+        {
+            card.gameObject.transform.SetParent(transform, false);//встваить текуший кардБед
+        }
+
+        card.gameObject.transform.Translate(new Vector3(0,
+             -CardIndentation)); //подвинуть на отступ вниз
+        
+        _cards.Add(card);//добавить карту в список
+    }
+
+    public void OnDrop(PointerEventData eventData)//при сбросе над бед
+    {
+        var item = Card.dragItem; //взятрь карту которая в полете
+        if (item != null)//если не ноль
+        {
+            if(_cards.Count>0)//если в списке карт этого обьекта больше 0
+            {
+                item.transform.SetParent(_cards[_cards.Count - 1].gameObject.transform);//то карта в карту
+            }
+            else//иначе
+            {
+                item.transform.SetParent(transform);// в трансформ этого обьекта
+            }
         }
     }
 }
