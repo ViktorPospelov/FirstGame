be00f1d5f31ae534e247bbb1ab00e20d32ec2559
Viktor <vik.posp@gmail.com>
2023-02-25 20:48:17 +0500

    Update FinishBed.cs

--- MyYaGame/Klondike/Assets/Script/Game/Bed/FinishBed.cs
+++ MyYaGame/Klondike/Assets/Script/Game/Bed/FinishBed.cs
@@ -6,10 +6,25 @@ using UnityEngine.EventSystems;
 
 public class FinishBed : CardBed
 {
+    private CardSuit _cardSuit;
     public override void OnDrop(PointerEventData eventData)
     {
         base.OnDrop(eventData);
         
         GetCard(eventData.pointerDrag).Indent = 0f;
+    }
+    public override bool CheckCanMove(PointerEventData eventData)
+    {
+        GetCards();
+        if (_cards.Count == 0)
+        {
+            if (GetCard(eventData.pointerDrag).CardItem.CardDignity == 14) return true;
+        }
+
+        if (_cards.Last().CardItem.CardDignity + 1 == GetCard(eventData.pointerDrag).CardItem.CardDignity)
+                return true;
+        if (_cards.Count == 1 && GetCard(eventData.pointerDrag).CardItem.CardDignity == 2) return true;
+        
+        return false;
     }
 }
